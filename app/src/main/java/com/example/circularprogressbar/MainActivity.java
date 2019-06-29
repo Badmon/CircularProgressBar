d8c11b6f63b3c29b819ce94fb715291a80184170
Badmon <jleonh@autonoma.edu.pe>
2019-06-29 02:50:31 -0500

    Commit

--- app/src/main/java/com/example/circularprogressbar/MainActivity.java
+++ app/src/main/java/com/example/circularprogressbar/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.circularprogressbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        //simulamos inicio proceso
+        progressBar.setVisibility(View.VISIBLE);
+
+        //simulamos fin proceso
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+            }
+        },6000);
+    }
+
+    private void init() {
+        this.progressBar = findViewById(R.id.progressBar);
+
     }
 }
